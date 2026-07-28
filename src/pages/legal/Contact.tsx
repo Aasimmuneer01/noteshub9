5fe2735e80773f130750cddd1c7c1c5e77c3fbd6
Aasimmuneer01 <aasimmuneer349@gmail.com>
2026-07-28 17:55:18 +0530

    Update Contact.tsx

--- src/pages/legal/Contact.tsx
+++ src/pages/legal/Contact.tsx
@@ -3,8 +3,8 @@ import LegalLayout from '../../components/LegalLayout';
 export default function Contact() {
   return (
     <LegalLayout title="Contact Us">
-      <p>Email: contact@eduplatform.com</p>
-      <p>WhatsApp: +1234567890</p>
+      <p>Email: noteshub9.official@gmail.com</p>
+      <p>WhatsApp: Unavailable</p>
       {/* Add form later */}
     </LegalLayout>
   );
