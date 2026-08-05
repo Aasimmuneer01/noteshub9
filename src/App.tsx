11ce120ff4ba0441c5afbcecbef190dfc0416dc0
Aasimmuneer01 <aasimmuneer349@gmail.com>
2026-08-05 18:16:32 +0530

    feat: add broadcast notification component
    Integrate the BroadcastNotification component into the main application layout to display global announcements to users.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { db } from './firebase/config';
 import Navbar from './components/Navbar';
 import BottomNav from './components/BottomNav';
 import UnreadNotification from './components/UnreadNotification';
+import BroadcastNotification from './components/BroadcastNotification';
 import Home from './pages/Home';
 import Resources from './pages/Resources';
 import PDFViewer from './components/PDFViewer';
@@ -89,6 +90,7 @@ function MainLayout() {
           });
         }
       }} />}
+      <BroadcastNotification />
       <UnreadNotification />
       <Navbar />
       <main className="flex-1 overflow-auto pt-16">
